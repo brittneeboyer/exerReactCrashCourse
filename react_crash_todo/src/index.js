fa542d21b917449b043d6bbb5213aede9b41d681
Brittnee Boyer <brittneewestra98@gmail.com>
2021-12-13 17:57:19 -0700

    commit

---
+++ react_crash_todo/src/index.js
@@ -0,0 +1,15 @@
+// https://www.youtube.com/watch?v=sBws8MSXN7A&t=1643s
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
